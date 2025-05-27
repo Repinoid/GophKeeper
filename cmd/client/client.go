e96014d8bb593983109be7ca844a50e1d96a2453
“Naeel” <tazet@narod.ru>
2025-05-27 15:39:13 +0400

    client cli begin

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
-	"log"
+	"errors"
+	"os"
+	"regexp"
+	"strings"
 
 	pb "gorsovet/cmd/proto"
+	"gorsovet/internal/models"
 	"gorsovet/internal/privacy"
 
 	"google.golang.org/grpc"
@@ -16,11 +20,11 @@ func main() {
 	ctx := context.Background()
 	err := initClient(ctx)
 	if err != nil {
-		log.Fatal(err)
+		models.Sugar.Fatal(err)
 	}
 
 	if err := run(ctx); err != nil {
-		log.Fatal(err)
+		models.Sugar.Info(err)
 	}
 }
 
@@ -29,31 +33,83 @@ func run(ctx context.Context) (err error) {
 	// устанавливаем соединение с сервером
 	tlsCreds, err := privacy.LoadClientTLSCredentials("../tls/public.crt")
 	if err != nil {
-		log.Fatal("cannot load TLS credentials: ", err)
+		models.Sugar.Fatalf("cannot load TLS credentials: ", err)
 	}
 	conn, err := grpc.NewClient(gPort, grpc.WithTransportCredentials(tlsCreds))
 
-	//	conn, err := grpc.NewClient(gPort, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatal(err)
+		models.Sugar.Fatal(err)
 	}
 	defer conn.Close()
 
-	// client := pb.NewGkeeperClient(conn)
-	// req := &pb.RegisterRequest{Username: "n77", Password: "passw"}
-	// resp, err := client.RegisterUser(ctx, req)
-	// log.Printf("%+v", resp)
-
 	client := pb.NewGkeeperClient(conn)
-	req := &pb.LoginRequest{Username: "n77", Password: "passw"}
+
+	if registerFlag != "" {
+		str := strings.ReplaceAll(registerFlag, " ", "")
+		args := strings.Split(str, ",")
+		if len(args) != 2 {
+			return errors.New("wrong number of arguments, should be <username, password>")
+		}
+		re := regexp.MustCompile("[0-9][a-z][A-Z]+")
+		if !re.MatchString(args[0]) {
+			return errors.New("wrong username, only letters & digits allowed [0-9][a-z][A-Z]")
+		}
+		err = AddUser(ctx, client, args[0], args[1])
+		return
+	}
+
+	if loginFlag != "" {
+		str := strings.ReplaceAll(loginFlag, " ", "")
+		args := strings.Split(str, ",")
+		if len(args) != 2 {
+			return errors.New("wrong number of arguments, should be <username, password>")
+		}
+		err = Login(ctx, client, args[0], args[1])
+		return
+	}
+	if putTextFlag != "" {
+		err = PutText(ctx, client, putTextFlag)
+		return
+	}
+
+	return
+}
+
+func AddUser(ctx context.Context, client pb.GkeeperClient, username, password string) (err error) {
+	req := &pb.RegisterRequest{Username: username, Password: password, Metadata: metaFlag}
+	resp, err := client.RegisterUser(ctx, req)
+	if err != nil {
+		return
+	}
+	models.Sugar.Debugf("%+v", resp)
+	return
+}
+
+func Login(ctx context.Context, client pb.GkeeperClient, username, password string) (err error) {
+	req := &pb.LoginRequest{Username: username, Password: password, Metadata: metaFlag}
 	resp, err := client.LoginUser(ctx, req)
-	log.Printf("%+v", resp)
-
+	if err != nil {
+		return
+	}
 	token := resp.GetToken()
+	if token == "" {
+		return errors.New("login did not return token")
+	}
+	os.Setenv("Token", token)
+	models.Sugar.Debugf("%+v", resp.Reply)
+	return
+}
 
-	reqtxt := &pb.PutTextRequest{Token: token, Textdata: "12345", Metadata: "metta"}
+func PutText(ctx context.Context, client pb.GkeeperClient, text string) (err error) {
+
+	token, exists := os.LookupEnv("Token")
+	if !exists {
+		return errors.New("no token")
+	}
+
+	reqtxt := &pb.PutTextRequest{Token: token, Textdata: text, Metadata: metaFlag}
 	respt, err := client.PutText(ctx, reqtxt)
-	log.Printf("%+v %v\n", respt, err)
+	models.Sugar.Debugf("%+v", respt.Reply)
 
 	return
 }
---
+++ cmd/client/precookClient.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"gorsovet/internal/models"
+	"log"
+
+	"go.uber.org/zap"
+)
+
+var metaFlag, registerFlag, loginFlag, putTextFlag, putFileFlag string
+
+func initClient(ctx context.Context) (err error) {
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal("cannot initialize zap")
+	}
+	defer logger.Sync()
+	models.Sugar = *logger.Sugar()
+
+	flag.StringVar(&metaFlag, "meta", "", "metadata, -meta=\"...metadata text...\"")
+	flag.StringVar(&registerFlag, "register", "", "register new user, -register=\"userName,password\" divided by comma")
+	flag.StringVar(&loginFlag, "login", "", "login to Server, -login=\"userName,password\" divided by comma")
+	flag.StringVar(&putFileFlag, "putfile", "", "put file to storage, -putfile=\"filePath/filename\"")
+	flag.StringVar(&putTextFlag, "puttext", "", "put text to storage, -puttext=\"...your text...\"")
+	flag.Parse()
+
+	if metaFlag == "" && registerFlag == "" && loginFlag == "" && putFileFlag == "" {
+		return errors.New("no any flag")
+	}
+
+	return
+}
